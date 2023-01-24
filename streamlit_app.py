cb33ff1c6371cff05fd41e44b7f8fbb9ac5b2d80
Benjamin Gaillard <122609660+Benjamin19Gaillard@users.noreply.github.com>
2023-01-24 16:39:04 +0100

    title

--- streamlit_app.py
+++ streamlit_app.py
@@ -97,6 +97,8 @@ if streamlit.button('Add a Fruit to the List'):
   my_cnx.close()
   streamlit.text(back_from_function)
 
+ streamlit.header("View Our Fruit List - Add Your Favorites!")
+  
 # Add a button to load the fruit
 if streamlit.button('Get Fruit List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
