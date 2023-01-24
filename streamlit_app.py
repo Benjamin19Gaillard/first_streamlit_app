2a440a8a4c9104010ef4e7508560be5887968ebb
Benjamin Gaillard <122609660+Benjamin19Gaillard@users.noreply.github.com>
2023-01-24 16:37:39 +0100

    fix wording

--- streamlit_app.py
+++ streamlit_app.py
@@ -102,4 +102,4 @@ if streamlit.button('Get Fruit List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
   my_cnx.close()
-  stremlit.dataframe(my_data_rows)
+  streamlit.dataframe(my_data_rows)
